01ace9bb49c22cb7afd7e1e163e53aeb3c66c66d
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-06 20:42:10 -0300

    update

--- server.js
+++ server.js
@@ -14,15 +14,9 @@ connectDB();
 const app = express();
 
 // Configuración de CORS
-const corsOptions = {
-  origin: 'https://front-party-kids.vercel.app', // URL exacta del frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos, incluidas las solicitudes preflight
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
-  credentials: true, // Permitir el uso de cookies/autenticación
-};
 
 // Middleware para CORS
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Middleware para analizar datos JSON
 app.use(express.json());
