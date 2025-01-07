ca1bb7364174017137231f69070e9000ad6c8990
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-06 21:35:25 -0300

    update

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
 const connectDB = require('./config/db');
 const bookingRoutes = require('./routes/bookingRoutes');
 
@@ -13,22 +12,22 @@ connectDB();
 // Inicializar la app de Express
 const app = express();
 
-// Configuración de CORS
-const corsOptions = {
-  origin: 'https://front-party-kids.vercel.app', // URL exacta del frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos, incluidas las solicitudes preflight
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
-  credentials: true, // Permitir el uso de cookies/autenticación
-};
-
-// Middleware para CORS
-app.use(cors(corsOptions));
+// Configuración de CORS manualmente
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Permite cualquier origen, ajusta esto a tu frontend
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE" // Métodos permitidos
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Cabeceras permitidas
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Responde con 200 OK para solicitudes preflight
+  }
+  next(); // Continuar con el siguiente middleware
+});
 
 // Middleware para analizar datos JSON
 app.use(express.json());
-
-// Habilitar solicitudes preflight (para DELETE, POST, PUT, etc.)
-app.options('*', cors(corsOptions));
 
 // Rutas de la API
 app.use('/api/bookings', bookingRoutes);
