5a8a02a55b869d707e29142d2699d82c32210dca
juanchoalcoba <juanchoalcoba@gmail.com>
2025-01-08 11:09:44 -0300

    update

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ const corsOptions = {
   app.use(cors(corsOptions));
   
   // Middleware para manejar las preflight requests (OPTIONS)
-
+  app.all('/*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE','OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  
+    next();
+  });
 
 
 // Middleware para analizar datos JSON
