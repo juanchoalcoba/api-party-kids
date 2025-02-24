1e7ae9b63ce783647d60994f730243f94f10f8d9
juanchoalcoba <juanchoalcoba@gmail.com>
2025-02-24 18:54:47 -0300

    update

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -99,28 +99,6 @@ router.put('/', async (req, res) => {
   }
 });
 
-router.put('/', async (req, res) => {
-  const { name } = req.query;  // Recibimos el 'name' desde los parámetros de la URL
-
-  try {
-    // Intentamos encontrar y actualizar la reserva que coincida con el 'name'
-    const result = await Booking.updateOne(
-      { name: name },
-      { $set: { viewedByAdmin: true } } // Actualizamos el campo 'confirmed' a true
-    );
-
-    // Verificamos si no se encontró ningún documento
-    if (result.nModified === 0) {
-      return res.status(404).json({ message: 'Reserva no encontrada o ya leida' });
-    }
-
-    // Si la actualización fue exitosa
-    res.status(200).json({ message: 'Reserva leida!!!' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error leyendo la reserva', error });
-  }
-});
-
 
 
 
