141171414374b105a454dfff7eb9e6e1dd7ee770
juanchoalcoba <juanchoalcoba@gmail.com>
2025-03-13 09:47:01 -0300

    update

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -100,13 +100,22 @@ router.put('/', async (req, res) => {
 });
 
 // Ruta PUT para marcar una reserva como vista
-router.patch('/:id', async (req, res) => {
-  const { id } = req.params;
+router.patch('/', async (req, res) => {
+  const { date } = req.body;  // Recibimos el nombre de la reserva en el cuerpo de la solicitud
+
   try {
-    await Booking.findByIdAndUpdate(id);
-    res.json({ message: 'Tarea eliminada' });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al eliminar la tarea' });
+    const result = await Booking.updateOne(
+      { name: date },
+      { $set: { viewedByAdmin: true } }
+    );
+
+    if (result.nModified === 0) {
+      return res.status(404).json({ message: 'Reserva no encontrada o ya leída' });
+    }
+
+    res.status(200).json({ message: 'Reserva marcada como leída' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error actualizando la reserva', error });
   }
 });
 
